7a766b785abc033d49f5d4463452bd681af6ed20
Aaron Parker <aaron@stealthpuppy.com>
2025-10-28 20:59:41 +1100

    Refactor and extend documentation generation script
    Replaces legacy comment blocks with regions, adds YAML front matter to all generated markdown files, and introduces new documentation for shortcuts and start menu. Also refines logic for file and shortcut processing and removes redundant registry type handling from tables.

--- ci/New-Docs.ps1
+++ ci/New-Docs.ps1
@@ -1,16 +1,22 @@
-
+#Requires -Module "MarkdownPS"
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
     [System.String] $Path
 )
-$Configs = Get-ChildItem -Path "$Path\*.json" -Recurse
+$Configs = Get-ChildItem -Path "$Path/*.json" -Recurse
 
-<#
-    Registry settings
-#>
+$Layout = @"
+---
+layout: doc
+---
+
+"@
+
+#region Registry settings
 $OutFile = [System.IO.Path]::Combine("docs/registry.md")
-$Markdown = New-MDHeader -Text "Registry Settings" -Level 1
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Registry Settings" -Level 1
 $Markdown += "`n"
 foreach ($file in $Configs) {
 
@@ -49,12 +55,12 @@ foreach ($file in $Configs) {
     }
 }
 ($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
 
-<#
-    Capabilities and features
-#>
+#region Capabilities and features
 $OutFile = [System.IO.Path]::Combine("docs/features.md")
-$Markdown = New-MDHeader -Text "Removed Capabilities and Features" -Level 1
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Removed Capabilities and Features" -Level 1
 $Markdown += "`n"
 foreach ($file in $Configs) {
 
@@ -66,9 +72,6 @@ foreach ($file in $Configs) {
         $Table = [PSCustomObject]@{
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
-        }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
         }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
@@ -89,9 +92,6 @@ foreach ($file in $Configs) {
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
         }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
-        }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
         $Markdown += $json.Features.Disable | ForEach-Object {
@@ -111,9 +111,6 @@ foreach ($file in $Configs) {
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
         }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
-        }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
         $Markdown += $json.Packages.Remove | ForEach-Object {
@@ -125,12 +122,12 @@ foreach ($file in $Configs) {
     }
 }
 ($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
 
-<#
-    Paths
-#>
+#region Paths
 $OutFile = [System.IO.Path]::Combine("docs/paths.md")
-$Markdown = New-MDHeader -Text "Removed Paths" -Level 1
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Removed Paths" -Level 1
 $Markdown += "`n"
 foreach ($file in $Configs) {
 
@@ -144,9 +141,6 @@ foreach ($file in $Configs) {
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
         }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
-        }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
         $Markdown += $json.Paths.Remove | ForEach-Object {
@@ -158,12 +152,12 @@ foreach ($file in $Configs) {
     }
 }
 ($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
 
-<#
-    Services
-#>
+#region Services
 $OutFile = [System.IO.Path]::Combine("docs/services.md")
-$Markdown = New-MDHeader -Text "Services" -Level 1
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Services" -Level 1
 $Markdown += "`n"
 foreach ($file in $Configs) {
 
@@ -176,9 +170,6 @@ foreach ($file in $Configs) {
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
         }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
-        }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
         $Markdown += $json.Services.Enable | ForEach-Object {
@@ -190,26 +181,23 @@ foreach ($file in $Configs) {
     }
 }
 ($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
 
-<#
-    Files
-#>
+#region Files
 $OutFile = [System.IO.Path]::Combine("docs/files.md")
-$Markdown = New-MDHeader -Text "Files" -Level 1
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Files" -Level 1
 $Markdown += "`n"
 foreach ($file in $Configs) {
 
     $json = Get-Content -Path $file.FullName | ConvertFrom-Json
-    if ($null -ne $json.Files.Copy) {
+    if ($json.Files.Copy.Count -gt 0) {
         $Markdown += New-MDHeader -Text $file.Name -Level 2
         $Markdown += "`n"
         $Markdown += "**$($json.Description)**`n`n"
         $Table = [PSCustomObject]@{
             "Minimum build" = $json.MinimumBuild
             "Maximum build" = $json.MaximumBuild
-        }
-        if ($null -ne $json.Registry.Type) {
-            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.Registry.Type
         }
         $Markdown += $Table | New-MDTable -Shrink
         $Markdown += "`n"
@@ -218,3 +206,63 @@ foreach ($file in $Configs) {
     }
 }
 ($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
+
+#region Shortcuts
+$OutFile = [System.IO.Path]::Combine("docs/shortcuts.md")
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Shortcuts" -Level 1
+$Markdown += "`n"
+foreach ($file in $Configs) {
+
+    $json = Get-Content -Path $file.FullName | ConvertFrom-Json
+    if ($json.Shortcuts.Count -gt 0) {
+        $Markdown += New-MDHeader -Text $file.Name -Level 2
+        $Markdown += "`n"
+        $Markdown += "**$($json.Description)**`n`n"
+        $Table = [PSCustomObject]@{
+            "Minimum build" = $json.MinimumBuild
+            "Maximum build" = $json.MaximumBuild
+        }
+        $Markdown += $Table | New-MDTable -Shrink
+        $Markdown += "`n"
+        $Markdown += $json.Shortcuts | New-MDTable -Shrink
+        $Markdown += "`n"
+    }
+}
+($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
+
+#region Start Menu
+$OutFile = [System.IO.Path]::Combine("docs/startmenu.md")
+$Markdown = $Layout
+$Markdown += New-MDHeader -Text "Start Menu" -Level 1
+$Markdown += "`n"
+foreach ($file in $Configs) {
+
+    $json = Get-Content -Path $file.FullName | ConvertFrom-Json
+    if ($null -ne $json.StartMenu) {
+        $Markdown += New-MDHeader -Text $file.Name -Level 2
+        $Markdown += "`n"
+        $Markdown += "**$($json.Description)**`n`n"
+        $Table = [PSCustomObject]@{
+            "Minimum build" = $json.MinimumBuild
+            "Maximum build" = $json.MaximumBuild
+        }
+        if ($null -ne $json.StartMenu.Type) {
+            $Table | Add-Member -MemberType "NoteProperty" -Name "Type" -Value $json.StartMenu.Type
+        }
+        if ($null -ne $json.StartMenu.Feature) {
+            $Table | Add-Member -MemberType "NoteProperty" -Name "Feature" -Value $json.StartMenu.Feature
+        }
+        $Markdown += $Table | New-MDTable -Shrink
+        $Markdown += "`n"
+        $Markdown += $json.StartMenu.Exists | New-MDTable -Shrink
+        $Markdown += $json.StartMenu.NotExists | New-MDTable -Shrink
+        $Markdown += $json.StartMenu.Windows10 | New-MDTable -Shrink
+        $Markdown += $json.StartMenu.Windows11 | New-MDTable -Shrink
+        $Markdown += "`n"
+    }
+}
+($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
+#endregion
